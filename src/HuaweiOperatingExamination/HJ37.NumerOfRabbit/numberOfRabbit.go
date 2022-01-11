c2dd7236ab55cf8c3f0602e0f87098dfa86b4bde
yemuc1210 <1006537138@qq.com>
2022-01-11 11:20:50 +0800

    2022.0111

--- src/HuaweiOperatingExamination/HJ37.NumerOfRabbit/numberOfRabbit.go
+++ src/HuaweiOperatingExamination/HJ37.NumerOfRabbit/numberOfRabbit.go
@@ -1,15 +1,20 @@
-#include <iostream>
-using namespace std;
+package main
 
-int main(){
-    int m;
-    while(cin>>m){
-        int a=1,b=0,c=0;//a:一个月兔子数，b：两个月兔子数，c：三个月兔子个数
-        while(--m){//每过一个月兔子数变化
+import "fmt"
+
+func main(){
+    var m int
+    for {
+		n,_ := fmt.Scanln(&m)
+		if n==0 {
+			break
+		}
+        var a,b,c =1,0,0 //a:一个月兔子数，b：两个月兔子数，c：三个月兔子个数
+        for m-=1;m>0;m--{//每过一个月兔子数变化
             c+=b;
             b=a;
             a=c;
         }
-        cout<<a+b+c<<endl;
+        fmt.Println(a+b+c)
     }
 }
