5f73a88beee7cb910205598eea81c9e3621f6f80
yemuc1210 <1006537138@qq.com>
2022-01-14 10:46:08 +0800

    2022.01.14

--- src/leetcode/0373.KSmalestPairs/kSmallestPairs.go
+++ src/leetcode/0373.KSmalestPairs/kSmallestPairs.go
@@ -12,60 +12,30 @@ package lt373
 import (
     "container/heap"
 )
-
-func kSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
-    arrays := generateTwoArrs(nums1, nums2)
-    if len(arrays) <= k {
-        return arrays
+func kSmallestPairs(nums1, nums2 []int, k int) (ans [][]int) {
+    m, n := len(nums1), len(nums2)
+    h := hp{nil, nums1, nums2}
+    for i := 0; i < k && i < m; i++ {
+        h.data = append(h.data, pair{i, 0})
     }
-
-    heap.Init(&arrays)
-    result := make(DoubleArray, k)
-    for i := 0; i < k; i++ {
-        result[i] = (heap.Pop(&arrays)).([]int)
-    }
-
-    return result
-}
-
-func generateTwoArrs(nums1, nums2 []int) DoubleArray {
-    arrs := make(DoubleArray, 0, len(nums1) * len(nums2))
-    for n := 0; n < len(nums1); n++ {
-        for m := 0; m < len(nums2); m++ {
-            arrs = append(arrs, []int{nums1[n], nums2[m]})
+    for h.Len() > 0 && len(ans) < k {
+        p := heap.Pop(&h).(pair)
+        i, j := p.i, p.j
+        ans = append(ans, []int{nums1[i], nums2[j]})
+        if j+1 < n {
+            heap.Push(&h, pair{i, j + 1})
         }
     }
-
-    return arrs
+    return
 }
 
-// 实现container/heap的接口
-type DoubleArray [][]int
-
-func (doubleArray *DoubleArray) Push(x interface{}) {
-    *doubleArray = append(*doubleArray, x.([]int))
+type pair struct{ i, j int }
+type hp struct {
+    data         []pair
+    nums1, nums2 []int
 }
-
-func (doubleArray *DoubleArray) Pop() interface{} {
-    length := len(*doubleArray)
-    val := (*doubleArray)[length - 1]
-    *doubleArray = (*doubleArray)[: length - 1]
-
-    return val
-}
-
-func (doubleArray DoubleArray) Len() int {
-    return len(doubleArray)
-}
-
-func (doubleArray DoubleArray) Less(i int, j int) bool {
-    sum1 := doubleArray[i][0] + doubleArray[i][1]
-    sum2 := doubleArray[j][0] + doubleArray[j][1]
-    return sum1 <= sum2
-}
-
-func (doubleArray DoubleArray) Swap(i int, j int) {
-    doubleArray[i], doubleArray[j] = doubleArray[j], doubleArray[i]
-}
-
-
+func (h hp) Len() int            { return len(h.data) }
+func (h hp) Less(i, j int) bool  { a, b := h.data[i], h.data[j]; return h.nums1[a.i]+h.nums2[a.j] < h.nums1[b.i]+h.nums2[b.j] }
+func (h hp) Swap(i, j int)       { h.data[i], h.data[j] = h.data[j], h.data[i] }
+func (h *hp) Push(v interface{}) { h.data = append(h.data, v.(pair)) }
+func (h *hp) Pop() interface{}   { a := h.data; v := a[len(a)-1]; h.data = a[:len(a)-1]; return v }
