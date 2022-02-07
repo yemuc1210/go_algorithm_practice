1f1058923199e3287313856548af23b2f074754b
yemuc1210 <1006537138@qq.com>
2022-02-07 11:38:05 +0800

    2022.2.7

---
+++ src/leetcode/1405.LongestHappyString/longestHappyString.go
@@ -0,0 +1,28 @@
+package lt1405
+
+import "sort"
+
+func longestDiverseString(a, b, c int) string {
+    ans := []byte{}
+    cnt := []struct{ c int; ch byte }{{a, 'a'}, {b, 'b'}, {c, 'c'}}
+    for {
+        sort.Slice(cnt, func(i, j int) bool { return cnt[i].c > cnt[j].c })
+        hasNext := false
+        for i, p := range cnt {
+            if p.c <= 0 {
+                break
+            }
+            m := len(ans)
+            if m >= 2 && ans[m-2] == p.ch && ans[m-1] == p.ch {
+                continue
+            }
+            hasNext = true
+            ans = append(ans, p.ch)
+            cnt[i].c--
+            break
+        }
+        if !hasNext {
+            return string(ans)
+        }
+    }
+}
