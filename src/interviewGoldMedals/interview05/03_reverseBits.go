0a1c1e9d7cd7e5ff4327c62c3b890b2fd6fe583a
yemuc1210 <1006537138@qq.com>
2022-04-23 10:47:30 +0800

    面试题 05.03. 翻转数位

--- src/interviewGoldMedals/interview05/03_reverseBits.go
+++ src/interviewGoldMedals/interview05/03_reverseBits.go
@@ -6,5 +6,25 @@ package interview05
 请编写一个程序，找出你能够获得的最长的一串1的长度。
 */
 func reverseBits(num int) int {
-
+	cur := 0 // 当前位置连续1的个数  遇0归零
+	insert := 0  // 当前位置变为1，往前数连续1的最大个数，遇到0 cur+1,遇到1+1
+	res := 1
+	for i:=0;i<32;i++{
+		if num & 1 == 1{ // 倒数第i位是否为1    每次num都会移位
+			cur += 1
+			insert += 1
+		}else{
+			insert = cur+1
+			cur = 0
+		}
+        num >>= 1   // 右移
+		res = max(res,insert)
+	}
+	return res
+}
+func max(a,b int) int {
+	if a>b {
+		return a
+	}
+	return b
 }
