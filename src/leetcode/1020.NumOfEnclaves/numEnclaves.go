7f1126822fc2f2710acc2ebbc48e6361e9effcfc
yemuc1210 <1006537138@qq.com>
2022-04-27 08:49:30 +0800

    1020. 飞地的数量

--- src/leetcode/1020.NumOfEnclaves/numEnclaves.go
+++ src/leetcode/1020.NumOfEnclaves/numEnclaves.go
@@ -1,37 +1,39 @@
 package lt1020
+func numEnclaves(grid [][]int) int {
+    var dirs = [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    m,n := len(grid),len(grid[0])
 
-var dirs = []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-
-func numEnclaves(grid [][]int) (ans int) {
-    m, n := len(grid), len(grid[0])
-    vis := make([][]bool, m)
-    for i := range vis {
-        vis[i] = make([]bool, n)
-    }
-    var dfs func(int, int)
-    dfs = func(r, c int) {
-        if r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0 || vis[r][c] {
-            return
+    var dfs func(i,j int) 
+    dfs = func(i,j int) {
+        // 边界
+        if i<0 || i>=m || j<0 || j>=n || grid[i][j]==0 || grid[i][j]==2{
+            return 
         }
-        vis[r][c] = true
-        for _, d := range dirs {
-            dfs(r+d.x, c+d.y)
+        // 访问
+        grid[i][j] = 2
+        for _,dir := range dirs {
+            x,y := i+dir[0],j+dir[1]
+            dfs(x,y)
         }
     }
-    for i := range grid {
-        dfs(i, 0)
-        dfs(i, n-1)
+    //  从边界访问
+    for i:=range grid {
+        dfs(i,0)
+        dfs(i,n-1)
     }
-    for j := 1; j < n-1; j++ {
-        dfs(0, j)
-        dfs(m-1, j)
+    for j:=1;j<n-1;j++{
+        dfs(0,j)
+        dfs(m-1,j)
     }
-    for i := 1; i < m-1; i++ {
-        for j := 1; j < n-1; j++ {
-            if grid[i][j] == 1 && !vis[i][j] {
-                ans++
+    // 统计
+    var res int
+    for i:=1;i<m;i++{
+        for j:=1;j<n-1;j++{
+            // 判断是否访问过
+            if grid[i][j] ==1 {
+                res ++
             }
         }
     }
-    return
+    return res
 }
