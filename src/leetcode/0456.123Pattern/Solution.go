a3cd29298e57b5312e038f6d2889187d6afd988b
yemuc1210 <1006537138@qq.com>
2022-04-30 14:41:17 +0800

    456. 132 模式

--- src/leetcode/0456.123Pattern/Solution.go
+++ src/leetcode/0456.123Pattern/Solution.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+func find132pattern(nums []int) bool {
+	n:=len(nums)
+	if n < 3 {
+		return false
+	}
+	last := math.MinInt32  // 2
+	sk := []int{}  // 存3
+	for i:=n-1;i>=0;i--{
+		if nums[i]<last{
+			return true
+		}
+		for len(sk)>0 && sk[len(sk)-1]<nums[i] {
+			last = sk[len(sk)-1]
+			sk = sk[:len(sk)-1]
+		}
+		sk = append(sk, nums[i])
+	}
+	return false
+}
 func main(){
-	ss
+	nums := []int{3,1,4,2}
+	res := find132pattern(nums)
+	fmt.Println(res)
 }
--- src/leetcode/0456.123Pattern/Solution.java
+++ src/leetcode/0456.123Pattern/Solution.java
@@ -1,11 +1,32 @@
-给你一个整数数组 nums ，数组中共有 n 个整数。
-132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，
-并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
 
-如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
+// 给你一个整数数组 nums ，数组中共有 n 个整数。
+// 132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，
+// 并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+
+// 如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
 class Solution {
-    // 456. 132 模式
     public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        int last = Integer.MIN_VALUE; // 132中的2
+        Stack<Integer> sta = new Stack<>();// 用来存储132中的3
+        if(nums.length < 3)
+            return false;
+        for(int i=n-1; i>=0; i--){
 
+            if(nums[i] < last) // 若出现132中的1则返回正确值
+                return true;
+
+            // 若当前值大于或等于2则更新2（2为栈中小于当前值的最大元素）
+            while(!sta.isEmpty() && sta.peek() < nums[i]){
+                last = sta.pop();
+            }
+
+            // 将当前值压入栈中
+            sta.push(nums[i]);
+        }
+        return false;
     }
 }
