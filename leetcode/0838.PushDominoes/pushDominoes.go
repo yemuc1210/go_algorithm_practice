2c3bd1c09029808d6e50fcd4de1764604c6ee22a
yemuc1210 <1006537138@qq.com>
2022-05-09 11:02:11 +0800

    838. 推多米诺

---
+++ leetcode/0838.PushDominoes/pushDominoes.go
@@ -0,0 +1,45 @@
+package main
+
+
+func pushDominoes(dominoes string) string {
+    n := len(dominoes)
+    q := []int{}
+    time := make([]int, n)
+    for i := range time {
+        time[i] = -1
+    }
+    force := make([][]byte, n)
+    for i, ch := range dominoes {
+        if ch != '.' {
+            q = append(q, i)
+            time[i] = 0
+            force[i] = append(force[i], byte(ch))
+        }
+    }
+
+    ans := bytes.Repeat([]byte{'.'}, n)
+    for len(q) > 0 {
+        i := q[0]
+        q = q[1:]
+        if len(force[i]) > 1 {
+            continue
+        }
+        f := force[i][0]
+        ans[i] = f
+        ni := i - 1
+        if f == 'R' {
+            ni = i + 1
+        }
+        if 0 <= ni && ni < n {
+            t := time[i]
+            if time[ni] == -1 {
+                q = append(q, ni)
+                time[ni] = t + 1
+                force[ni] = append(force[ni], f)
+            } else if time[ni] == t+1 {
+                force[ni] = append(force[ni], f)
+            }
+        }
+    }
+    return string(ans)
+}
