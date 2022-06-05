7f9c9bbcd37e3df39b1c2312e35b2ba7766657fb
yemuc1210 <1006537138@qq.com>
2022-06-05 09:54:42 +0800

    478. 在圆内随机生成点

--- leetcode/0929.UniqueEmailAddresses/numUniqueEmails.go
+++ leetcode/0929.UniqueEmailAddresses/numUniqueEmails.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func numUniqueEmails(emails []string) int {
 	// 有效的电子邮件地址：本地名@域名
@@ -9,14 +12,29 @@ func numUniqueEmails(emails []string) int {
 	// 本地名有'+'，则忽略第一个加号后面所有内容
 	
 	res := 0
-
+	// m := make(map[string]int)
 	// 1. 判断有么@
 	for _,email := range emails {
 		exist := strings.ContainsRune(email,'@')
 		if !exist {
 			continue
 		}
-		// 
+		// 解析email
+		names := strings.Split(email,"@")
+		fmt.Println(names)
 	}
 	return res
 }
+
+func main() {
+	emails := []string{
+		"test.email+alex@leetcode.com",
+		"test.e.mail+bob.cathy@leetcode.com",
+		"testemail+david@lee.tcode.com"}
+	res := numUniqueEmails(emails)
+	fmt.Println(res)
+}
+
+// func main() {
+
+// }
